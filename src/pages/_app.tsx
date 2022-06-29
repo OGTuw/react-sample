ecfee434b4d0ff890a0bd4d71dd22c7b777296d0
ogatatsu <55011568+OGTuw@users.noreply.github.com>
2022-06-29 22:17:57 +0900

    :tada: 認証触ってみた

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,42 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import { supabase } from '../utils/supabase';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  const { pathname, push } = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  supabase.auth.onAuthStateChange((_, session) => {
+    if (session?.user && (pathname === '/sign-in' || pathname === '/sign-up')) {
+      push('/');
+    } else if (!session?.user && pathname !== '/sign-up') {
+      push('/sign-in');
+    }
+  });
+
+  useEffect(() => {
+    (async () => {
+      const user = supabase.auth.user();
+      if (user && (pathname === '/sign-in' || pathname === '/sign-up')) {
+        await push('/');
+      } else if (!user && pathname !== '/sign-up') {
+        await push('/sign-in');
+      }
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <>
+          <button onClick={() => supabase.auth.signOut()}>ログアウト</button>
+          <Component {...pageProps} />
+        </>
+      )}
+    </>
+  );
 }
-
-export default MyApp
