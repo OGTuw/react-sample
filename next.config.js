dedb6ba34a0a1111a3e26ad96e101efce8744e91
ogatatsu <55011568+OGTuw@users.noreply.github.com>
2022-06-29 21:19:40 +0900

    :heavy_plus_sign: supabaseを使えるようにする

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+
+  env: {
+    SUPABASE_URL: 'https://toekfuvfpfhxnulgtyzi.supabase.co',
+    SUPABASE_KEY:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvZWtmdXZmcGZoeG51bGd0eXppIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY1MDQ2NDEsImV4cCI6MTk3MjA4MDY0MX0.FRrj5KWFPoVUMwfC2zVLfy70gbHF6aOPOV3F3FuANlw',
+  },
+};
